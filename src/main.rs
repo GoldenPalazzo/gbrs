4c3cb9ab36431e3269f64cc6898c5f41117550d2
Francesco Palazzo <francesco@palazzo.me>
2026-02-16 13:40:02 +0100

    Passing filename as argument

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ mod memory;
 use crate::cpu::cpu::CPU;
 use crate::memory::memory::MemoryBus;
 
+use log::debug;
 use std::io::{self, Write};
+use std::env;
 
 fn wait_for_enter() {
     let mut input = String::new();
@@ -13,9 +15,14 @@ fn wait_for_enter() {
 
 fn main() -> std::io::Result<()> {
     env_logger::init();
+    let args: Vec<String> = env::args().collect();
+    debug!("args: {:#?}", args);
+    if args.len() < 2 {
+        panic!("No program provided!");
+    }
 
     let mut cpu = CPU::new();
-    let mut mem = MemoryBus::from_file("./example.gb");
+    let mut mem = MemoryBus::from_file(&args[1]);
     println!("Loaded cart {:?}", mem.cart.title);
     let mut bp = false;
     let bps = vec![0x20f];
