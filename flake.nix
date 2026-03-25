98c14a38eb6e767abb2b47bbc85ce7115c47bd9b
Francesco Palazzo <francesco@palazzo.me>
2026-03-25 12:59:30 +0100

    NixOS development files

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Flake for rust development";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+          cargo rustc rustfmt clippy rust-analyzer
+          # deps
+          pkg-config
+      ];
+      env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    };
+  };
+}
